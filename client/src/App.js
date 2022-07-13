470af6f4f48427d1ae5b97ff0b1c7fa9f397ac08
harishpariyar07 <harishpariyar07@users.noreply.github.com>
2022-07-13 22:48:42 +0545

    Restructured the whole layout of the page

--- client/src/App.js
+++ client/src/App.js
@@ -1,53 +1,35 @@
-<<<<<<< HEAD
-import { CCol, CContainer, CRow } from "@coreui/react";
+import logo from "./logo.svg";
 import "./App.css";
-import Sidebar from "./components/Sidebar";
+import "@coreui/coreui/dist/css/coreui.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Announcement from "./Screens/Announcement";
 import Header from "./components/Header";
-import Main from "./components/Main";
-
-function App() {
-  return (
-    <CContainer fluid className="p-0 m-0">
-      <CRow>
-        <CCol md={4} lg={3} className="sidebar-container px-0">
-          <Sidebar />
-        </CCol>
-        <CCol md={8} lg={9} xs={12} className="wrapper-container">
-          <CRow className="header-container">
-            <Header />
-          </CRow>
-          <CRow className="main-container px-2">
-            <Main />
-          </CRow>
-        </CCol>
-      </CRow>
-    </CContainer>
-  );
-=======
-import logo from './logo.svg'
-import './App.css'
-import { CContainer } from '@coreui/react'
-import '@coreui/coreui/dist/css/coreui.min.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Announcement from './Screens/Announcement'
-import { Routes, Route } from 'react-router-dom'
-import { AnnouncementUpdate } from './Screens'
+import Sidebar from "./components/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import { AnnouncementUpdate } from "./Screens";
+import { CContainer, CRow, CCol } from "@coreui/react";
 
 function App() {
   return (
     <>
-      <CContainer fluid>
-        <Routes>
-          <Route path="/admin/announcement" element={<Announcement />} />
-          <Route
-            path="/admin/announcement/edit/:id"
-            element={<AnnouncementUpdate />}
-          />
-        </Routes>
+      <CContainer fluid className="p-0 m-0">
+        <CRow>
+          <Sidebar />
+          <CCol md={8} lg={9} xs={12} className="wrapper-container p-0">
+            <CRow className="main-container">
+              <Routes>
+                <Route path="/admin/announcement" element={<Announcement />} />
+                <Route
+                  path="/admin/announcement/edit/:id"
+                  element={<AnnouncementUpdate />}
+                />
+              </Routes>
+            </CRow>
+          </CCol>
+        </CRow>
       </CContainer>
     </>
-  )
->>>>>>> 3b7466703d14beff7c40b5fc11044a347029863f
+  );
 }
 
-export default App
+export default App;
--- client/src/Screens/Announcement.jsx
+++ client/src/Screens/Announcement.jsx
@@ -1,101 +1,133 @@
-import React, { useEffect } from 'react'
-import { CRow, CCol, CButton, CTable, CContainer } from '@coreui/react'
-import { Link } from 'react-router-dom'
-import { useNavigate } from 'react-router-dom'
-import axios from 'axios'
+import React, { useEffect } from "react";
+import {
+  CRow,
+  CCol,
+  CButton,
+  CTable,
+  CContainer,
+  CTableHead,
+  CTableRow,
+  CTableHeaderCell,
+  CTableBody,
+  CTableDataCell,
+} from "@coreui/react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import Header from "../components/Header";
+import "../index.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faEdit, faPlus } from "@fortawesome/free-solid-svg-icons";
 
 function Announcement() {
-  const navigate = useNavigate()
-  const [announcements, setAnnouncements] = React.useState([])
+  const navigate = useNavigate();
+  const [announcements, setAnnouncements] = React.useState([]);
 
   //functions
   const createProductHandle = async () => {
-    const { data } = await axios.post('/announcement', {
-      heading: 'Default',
-      content: 'Default',
-    })
-    if (!data) return console.log('error')
-    navigate(`/admin/announcement/edit/${data._id}`)
-  }
+    const { data } = await axios.post("/announcement", {
+      heading: "Default",
+      content: "Default",
+    });
+    if (!data) return console.log("error");
+    navigate(`/admin/announcement/edit/${data._id}`);
+  };
   const deleteHandle = async (id) => {
     try {
-      const { data } = await axios.delete(`/announcement/${id}`)
+      const { data } = await axios.delete(`/announcement/${id}`);
 
-      if (!data) throw new Error('user not found')
+      if (!data) throw new Error("user not found");
 
       // setErr({ variant: 'success', msg: 'Product Deleted' })
 
-      window.location.reload()
+      window.location.reload();
     } catch (error) {
       // setErr({ variant: 'danger', msg: error.message })
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
   const fetchAnnouncements = async () => {
-    const { data } = await axios.get('/announcement')
+    const { data } = await axios.get("/announcement");
 
-    setAnnouncements(...announcements, data)
-  }
+    setAnnouncements(...announcements, data);
+  };
 
   useEffect(() => {
-    fetchAnnouncements()
-  }, [])
+    fetchAnnouncements();
+  }, []);
 
   return (
     <CContainer>
       <CRow className="align-items-center">
         <CCol xs="12">
-          <h3 className="text-center">Announcement</h3>
+          <Header text="Announcement" />
         </CCol>
-        <CCol style={{ textAlign: 'right' }}>
-          <CButton className="my-3" onClick={createProductHandle}>
-            <i className="fas fa-plus"> Create Product</i>
+        <CCol style={{ textAlign: "right" }}>
+          <CButton
+            className="my-3"
+            color="primary"
+            onClick={createProductHandle}
+          >
+            <FontAwesomeIcon icon={faPlus} className="px-1" />
+            Create Product
           </CButton>
         </CCol>
       </CRow>
-      <CTable striped bordered hover responsive className="table-sm3">
-        <thead>
-          <tr>
-            <th>Id</th>
-            <th>Heading</th>
-            <th>Type</th>
-            <th>Year</th>
-            <th>Content</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
+      <CTable
+        striped
+        bordered
+        hover
+        responsive
+        className="table-sm3 align-middle"
+      >
+        <CTableHead>
+          <CTableRow
+            style={{ backgroundColor: "rgba(85, 150, 230, 1)", color: "white" }}
+          >
+            <CTableHeaderCell>Id</CTableHeaderCell>
+            <CTableHeaderCell>Heading</CTableHeaderCell>
+            <CTableHeaderCell>Type</CTableHeaderCell>
+            <CTableHeaderCell>Year</CTableHeaderCell>
+            <CTableHeaderCell>Content</CTableHeaderCell>
+            <CTableHeaderCell></CTableHeaderCell>
+          </CTableRow>
+        </CTableHead>
+        <CTableBody color="light">
           {announcements.map((announcement) => (
-            <tr key={announcement._id}>
-              <td>{announcement._id}</td>
-              <td>{announcement.heading}</td>
-              <td>{announcement.type}</td>
-              <td>{announcement.year}</td>
-              <td>{announcement.content}</td>
-              <td>
+            <CTableRow key={announcement._id}>
+              <CTableDataCell>{announcement._id}</CTableDataCell>
+              <CTableDataCell>{announcement.heading}</CTableDataCell>
+              <CTableDataCell>{announcement.type}</CTableDataCell>
+              <CTableDataCell>{announcement.year}</CTableDataCell>
+              <CTableDataCell>{announcement.content}</CTableDataCell>
+              <CTableDataCell>
                 {/* <Link to={`/announcement/${product._id}`}> */}
-                <CButton
-                  className="my-3"
-                  onClick={() => deleteHandle(announcement._id)}
-                >
-                  <i className="fas fa-trash"> Delete</i>
-                </CButton>
-                {/* </Link> */}
-                <CButton
-                  className="my-3"
-                  onClick={() => {
-                    navigate(`/admin/announcement/edit/${announcement._id}`)
-                  }}
-                >
-                  <i className="fas fa-edit"> Edit</i>
-                </CButton>
-              </td>
-            </tr>
+                <CContainer className="announcement-btn-container">
+                  <CButton
+                    className="my-3"
+                    color="danger"
+                    onClick={() => deleteHandle(announcement._id)}
+                  >
+                    <FontAwesomeIcon icon={faTrash} />
+                  </CButton>
+                  {/* </Link> */}
+                  <CButton
+                    color="success"
+                    className="my-3"
+                    onClick={() => {
+                      navigate(`/admin/announcement/edit/${announcement._id}`);
+                    }}
+                  >
+                    <FontAwesomeIcon icon={faEdit} />
+                  </CButton>
+                </CContainer>
+              </CTableDataCell>
+            </CTableRow>
           ))}
-        </tbody>
+        </CTableBody>
       </CTable>
     </CContainer>
-  )
+  );
 }
 
-export default Announcement
+export default Announcement;
